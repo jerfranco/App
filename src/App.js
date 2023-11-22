e860096633649a162ea6b574887a292740363d22
Jeremiah Franco <jeremiahfranco3@gmail.com>
2023-11-22 11:13:18 -0700

    Final code for Web Portfolio

--- src/App.js
+++ src/App.js
@@ -1,24 +1,71 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import './style.css'; // Make sure to import your stylesheet
+
+// imported images and saved to variables
+import myImage from './IMG_4530.jpg';
+import githubImage from './github-mark-white.png';
+import linkedIn from './In-White-128@2x.png';
+import Gmail from './gmail-icon-transparent-4-3130333760.png';
+// import BoxSection from './BoxSection';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <header>
+      <nav>
+        <li>Jeremiah Franco</li>
+        <li id="major">Computer Science Student</li>
+      </nav>
+    </header>
+      <main>
+        <div id="start">
+          <div id="introduction">
+            {/* Display name */}
+		        <img src={myImage} alt="Jeremiah Franco" />
+            <div id="hello">
+              <h1 id="name">Jeremiah Franco</h1>
+            </div> 
+          </div>
+        </div>
+        {/* About me description */}
+        <div id="description">
+          <h2>About Me</h2>
+          <p id="about">I love dealing with computer systems, software and programming. 
+          I am currently a student studying Computer Science at Brigham
+          Young University - Idaho. I believe that with the technology rising and improving every single day,
+          we can use technology to help those who are in need and to make the world a better place!
+          I'm always willing to learn more about the programming world and am excited to get to help expand
+          the ever growing tech environment!</p>
+        </div>
+          {/* Contact me section */}
+          <div id="box">
+            <h1>Contact Me</h1>
+            <div id="section">
+            <div className="box-section">
+              {/* Github */}
+              <h2>GitHub</h2>
+              <a href="https://github.com/jerfranco?tab=repositories" target="blank">
+                <img src={githubImage} alt="GitHub Logo" />
+              </a>
+            </div>
+            <div className="box-section">
+              {/* LinkedIn */}
+              <h2>LinkedIn</h2>
+              <a href="https://www.linkedin.com/in/jeremiah-franco-896a46275/" target="_blank">
+                <img src={linkedIn} alt="LinkedIn Logo" />
+              </a>
+            </div>
+            <div className="box-section">
+              {/* Gmail */}
+              <h2>Gmail</h2>
+              <a href="mailto:jeremiahfranco3@gmail.com">
+                <img src={Gmail} alt="Gmail Logo" />
+              </a>
+            </div>
+            </div>
+          </div>
+      </main>
+    </>
   );
 }
 
